a996b3bc31dcdac9df1f6558d26fa6a5b06ca582
Mandi Rocket <mandi018@gmail.com>
2018-08-10 16:57:21 -0500

    Simple array to represent the adding of 3 heroes to the guild roster

--- hero.gd
+++ hero.gd
@@ -4,16 +4,23 @@ extends Node
 # var a = 2
 # var b = "textvar"
 
-var globalSoftCurrency = 123;
+var heroName = "Default Name"
+var heroLevel = 0
+var heroXp = 0
+var heroHp = 10
+var heroClass = "Wizard"
 
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	print(globalSoftCurrency) # can't figure out how to pass this to main 
 	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+func _init(heroNameVar):
+	print("New hero constructed! " + heroNameVar)
+	heroName = heroNameVar
--- main.gd
+++ main.gd
@@ -4,11 +4,25 @@ extends Node
 # var a = 2
 # var b = "textvar"
 
+const Hero = preload("hero.gd")
+
+var guildMembers = []
+
 func _ready():
+	# these should be global variables
 	$HUD.update_currency(150, 15)
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	
+	#attempting to populate the guild members array 
+	var heroQuantity = 3
+	for i in range(heroQuantity):
+		var newHero = Hero.new("hero #" + str(i));
+		guildMembers.append(newHero)
+		
+	print("Guild members are:")
+	for i in range(heroQuantity):
+		print(guildMembers[i].heroName)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -17,7 +31,6 @@ func _ready():
 
 
 func _on_Quests_pressed():
-
-	# get_tree().change_scene("res://Button.tscn");
+	get_tree().change_scene("res://Button.tscn");
 	print("test")
 	
