31756f8c628f26770ad30bba2cbd828686a3a445
Mandi Rocket <mandi018@gmail.com>
2018-08-10 22:11:16 -0500

    Heroes now have a random class and display their class and level below sprite

--- hero.gd
+++ hero.gd
@@ -9,12 +9,15 @@ var heroName = "Default Name"
 var heroLevel = 0
 var heroXp = 0
 var heroHp = 10
-var heroClass = "Wizard"
+var heroClass = "NONE"
+var currentRoom = 0
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	$field_name.text = heroName
+	$HBoxContainer/field_level.text = str(heroLevel)
+	$HBoxContainer/field_class.text = heroClass
 	pass
 	
 func set_name(newName):
@@ -22,6 +25,18 @@ func set_name(newName):
 	
 func set_level(newLevel):
 	heroLevel = newLevel
+	
+func set_class(newClass):
+	print("newClass number came in as: " + str(newClass))
+	if newClass == 1:
+		heroClass = "Wizard"
+	elif newClass == 2:
+		heroClass = "Rogue"
+	else:
+		heroClass = "Warrior"
+	
+func set_current_room(roomNumber):
+	currentRoom = roomNumber
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
--- main.gd
+++ main.gd
@@ -3,10 +3,10 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
 var guildMembers = []
 
 func _ready():
+	randomize()
 	# these should be global variables
 	$HUD.update_currency(150, 15)
 	# Called when the node is added to the scene for the first time.
@@ -17,15 +17,16 @@ func _ready():
 	var heroX = 100
 	var heroY = 100
 	for i in range(heroQuantity):
-		# var newHero = Hero.new("hero #" + str(i), 1);
 		var newHero = preload("res://hero.tscn").instance()
 		newHero.set_name("hero #" + str(i))
 		newHero.set_position(Vector2(heroX, heroY))
 		newHero.set_level(1)
+		newHero.set_class(randi()%3+1)
+		newHero.set_current_room(1)
 		guildMembers.append(newHero)
 		add_child(newHero) #put it on stage (child of main) 
 		heroX += 20
-		heroY += 25
+		heroY += 100
 		
 	#verify they were generated 
 	print("Guild members are:")
