be2932cfefda2c45eb7bec12c7a10fb6a5086e59
Mandi Rocket <mandi018@gmail.com>
2018-10-10 21:09:10 -0500

    Heads are now gender-based and stored as an array. Added a few more last names.

--- heroGenerator.gd
+++ heroGenerator.gd
@@ -1,7 +1,11 @@
 extends Node
+#heroGenerator.gd
 #makes a level 1 hero with random class and name
 
 var nameGenerator = load("res://nameGenerator.gd").new()
+var humanFemaleHeads = ["human_female_01.png", "human_female_02.png", "human_female_03.png", "human_female_04.png"]
+var humanMaleHeads = ["human_male_01.png", "human_male_02.png", "human_male_03.png", "human_male_04.png", "human_male_05.png"]
+var elfFemaleHeads = ["elf_female_01.png"]
 
 func _ready():
 	pass
@@ -21,73 +25,45 @@ func generate(destinationArray, classStr):
 	#random name 
 	#todo: pass species in addition to gender 
 	newHero.heroName = nameGenerator.generate(newHero.gender)
-	
+	#random head (these are gendered mostly so we don't end up with bearded ladies)
+	if (newHero.gender == "female"):
+		newHero.headSprite = humanFemaleHeads[randi() % humanFemaleHeads.size()]
+	else:
+		newHero.headSprite = humanMaleHeads[randi() % humanMaleHeads.size()]
+		
 	#random class
 	var randomClass = randi()%3+1 #1-4
-	var randomHead = randi()%3+1 #1-3
 	
 	if (classStr == "Wizard"):
 		newHero.heroClass = "Wizard"
-		#todo: must give an item to every slot or it'll crash (has no way to handle empty slots yet)
 		newHero.give_item("Novice's Robe") #chest
 		newHero.give_item("Simple Ring")
 		newHero.give_item("Cracked Staff")
 		newHero.give_item("Worn Canvas Sandals") #feet
 		newHero.give_item("Cloth Pants") #legs
-		newHero.shieldSprite = "none.png"
-		if (randomHead == 1):
-			newHero.headSprite = "human_female_01.png"
-		elif (randomHead == 2):
-			newHero.headSprite = "human_female_02.png"
-		else:
-			newHero.headSprite = "human_female_03.png"
 			
 	elif (classStr == "Rogue"):
 		newHero.heroClass = "Rogue"
-		
 		newHero.give_item("Rusty Knife")
 		newHero.give_item("Muddy Boots")
 		newHero.give_item("Blackguard's Chainmail Leggings")
 		newHero.give_item("Blackguard's Chainmail Vest")
-		
-		if (randomHead == 1):
-			newHero.headSprite = "human_female_04.png"
-		elif (randomHead == 2):
-			newHero.headSprite = "human_male_01.png"
-		else:
-			newHero.headSprite = "human_male_05.png"
-		
+
 	elif (classStr == "Warrior"):
 		newHero.heroClass = "Warrior"
-
 		newHero.give_item("Rusty Broadsword")
 		newHero.give_item("Reinforced Shield")
 		newHero.give_item("Muddy Boots")
 		newHero.give_item("Worn Ringmail Leg Guards")
 		newHero.give_item("Worn Ringmail Vest")
-		
-		if (randomHead == 1):
-			newHero.headSprite = "human_female_04.png"
-		elif (randomHead == 2):
-			newHero.headSprite = "human_male_02.png"
-		elif (randomHead == 3):
-			newHero.headSprite = "human_male_04.png"
-		newHero.shieldSprite = "shield01.png"
 	
 	elif (classStr == "Ranger"):
 		newHero.heroClass = "Ranger"
-		
 		newHero.give_item("Basic Bow")
 		newHero.give_item("Cloth Shirt")
 		newHero.give_item("Muddy Boots")
 		newHero.give_item("Cloth Pants")
 		
-		if (randomHead == 1):
-			newHero.headSprite = "human_female_01.png"
-		else:
-			newHero.headSprite = "human_male_05.png"
-		newHero.weapon1Sprite = "bow1.png" #bow
-		newHero.weapon2Sprite = "none.png"
 	else:
 		print("ERROR - BAD HERO CLASS TYPE")
 
