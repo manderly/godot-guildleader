8bd8697cd1efb1c0dde901d370a41d8c60eba359
Mandi Grant <mandi018@gmail.com>
2018-12-02 22:18:43 -0600

    Camera and device settings to make the game fit on phone screen

--- mainCamera.gd
+++ mainCamera.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 func _input(event):
 	if event is InputEventScreenDrag:
-		var newCoord = Vector2(0, get_offset().y - event.relative.y)
+		var newCoord = Vector2(16, get_offset().y - event.relative.y)
 		set_offset(newCoord) #attempt at "scrolling" camera (new way)
 		#self.move_local_y(event.relative.y) #inverse camera (old way)
 
