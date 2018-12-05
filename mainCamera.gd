bba5c2caca89dd77c62c0d20029d0cbe516d053e
Jonathan Burley <jonburley@gmail.com>
2018-12-05 15:45:57 -0600

    Removed acceleration from mainCamera. Adjusted camera values for new resolution.

--- mainCamera.gd
+++ mainCamera.gd
@@ -4,22 +4,22 @@ var isTouched = false
 var relativeY = 0
 var prevRelativeY = 0
 var acceleration = 1.2
-var deceleration = .4
+var deceleration = .25
 var velocity = 0
-const MAX_VELOCITY = 17
+const MAX_VELOCITY = 10
 
-func move_camera():
-	var newCoord = Vector2(16, get_offset().y - relativeY * velocity)
+func move_camera(yVal):
+	var newCoord = Vector2(-14, get_offset().y - yVal)
 	if (newCoord.y > global.mainScreenTop && newCoord.y < 0):
 		set_offset(newCoord) #attempt at "scrolling" camera (new way)
 		#self.move_local_y(event.relative.y) #inverse camera (old way)
 		global.cameraPosition = newCoord
 	elif (newCoord.y < global.mainScreenTop):
-		var topCoord = Vector2(16, global.mainScreenTop)
+		var topCoord = Vector2(-14, global.mainScreenTop)
 		set_offset(topCoord)
 		global.cameraPosition = topCoord
 	elif (newCoord.y > 0):
-		var bottomCoord = Vector2(16, 0)
+		var bottomCoord = Vector2(-14, 0)
 		set_offset(bottomCoord)
 		global.cameraPosition = bottomCoord
 
@@ -40,7 +40,7 @@ func _input(event):
 			
 		prevRelativeY = relativeY
 			
-		move_camera()
+		move_camera(event.relative.y)
 	elif !(event is InputEventScreenDrag or event is InputEventScreenTouch or event is InputEventMouseMotion):
 		isTouched = false
 
@@ -50,7 +50,7 @@ func _ready():
 func _process(delta: float) -> void:
 	if (!isTouched && velocity > 0):
 		velocity -= deceleration
-		move_camera()
+		move_camera(relativeY * velocity)
 	
 func set_cam_position():
 	set_offset(global.cameraPosition)
