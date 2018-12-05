408f8d6157486dd80b2ea45b3896dc2ce4f1504a
Mandi Grant <mandi018@gmail.com>
2018-12-05 09:48:21 -0600

    Took out camera print

--- mainCamera.gd
+++ mainCamera.gd
@@ -5,7 +5,6 @@ func _input(event):
 		var newCoord = Vector2(16, get_offset().y - event.relative.y)
 		set_offset(newCoord) #attempt at "scrolling" camera (new way)
 		#self.move_local_y(event.relative.y) #inverse camera (old way)
-		print("mainCamera.gd " + str(newCoord))
 		global.cameraPosition = newCoord
 
 func _ready():
